f6d87beae4a5d46303fff4879bb52984756ca0c3
Andrew Naplavkov <andrew.naplavkov@gmail.com>
2018-07-04 19:26:04 +0300

    Clean up

--- test/sparse_table.hpp
+++ test/sparse_table.hpp
@@ -35,12 +35,21 @@ TEST_CASE("sparse_table_max")
 
 TEST_CASE("sparse_table_gcd")
 {
-    const int a[] = {7, 2, 3, 0, 5, 10, 3, 12, 18};
-    step::sparse_table<int, step::gcd> tbl{std::begin(a), std::end(a)};
-    check_sparse_table(a, tbl);
-    CHECK(tbl.subarray(0, 2) == 1);
-    CHECK(tbl.subarray(1, 2) == 1);
-    CHECK(tbl.subarray(4, 1) == 5);
+    const int arr_a[] = {7, 2, 3, 0, 5, 10, 3, 12, 18};
+    step::sparse_table<int, step::gcd> tbl_a{std::begin(arr_a),
+                                             std::end(arr_a)};
+    check_sparse_table(arr_a, tbl_a);
+    CHECK(tbl_a.subarray(0, 3) == 1);
+    CHECK(tbl_a.subarray(1, 3) == 1);
+    CHECK(tbl_a.subarray(4, 2) == 5);
+
+    const int arr_b[] = {2, 3, 5, 4, 6, 8};
+    step::sparse_table<int, step::gcd> tbl_b{std::begin(arr_b),
+                                             std::end(arr_b)};
+    check_sparse_table(arr_b, tbl_b);
+    CHECK(tbl_b.subarray(0, 3) == 1);
+    CHECK(tbl_b.subarray(3, 3) == 2);
+    CHECK(tbl_b.subarray(2, 2) == 1);
 }
 
 #endif  // STEP_TEST_SPARSE_TABLE_HPP
